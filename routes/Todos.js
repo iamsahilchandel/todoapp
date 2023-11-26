77d6360a6d1a37654832e188578a84a87ba47226
sahil <rajputsahilchandel@gmail.com>
2023-11-26 21:03:02 +0530

    added get todo functionality

--- routes/Todos.js
+++ routes/Todos.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 //controllers
 const {createTodo} = require("../controllers/createTodo");
+const {getTodo} = require('../controllers/getTodo');
 
 //API Routes
 router.post("/createTodo", createTodo);
+router.get("/getTodo", getTodo);
 
 module.exports = router;
